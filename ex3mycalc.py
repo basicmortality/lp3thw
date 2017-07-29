79b9691e572aca1d0c1897808c0923135f5a6429
basicmortality <info@basicmortality.co.uk>
2017-07-29 20:26:01 +0100

    A tiny calc program with print statements

---
+++ ex3mycalc.py
@@ -0,0 +1,7 @@
+days_away = 7
+hours_away = days_away * 24
+seconds_away = hours_away * 3600
+
+print("We will be away for", days_away, "days.")
+print("We will be away for", hours_away, "hours.")
+print("We will be away for", seconds_away, "seconds.")
