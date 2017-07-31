41bf22cc4ea74b5b1ed20d9f7fd3fefb20114c55
basicmortality <info@basicmortality.co.uk>
2017-07-31 19:03:07 +0100

    ex9.py add, with #comments

---
+++ ex9.py
@@ -0,0 +1,16 @@
+days = "Mon Tue Wed Thu Fri Sat Sun" # this is a normal spaced string variable
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec" # this
+# sets 'months' as "new line spaced" 'Months'; note no spaces...
+# Zed put Jan on the first line after "Here are the m.." but I added a \n
+# to put all the months on new lines
+
+print("Here are the days: ", days) # print command wih var output
+print("Here are the months: ", months) # and again...
+
+#  and this next one prints out as much as you like, just how you type it
+print("""
+There's something going on here
+With the three double-qoutes.
+We'll be able to type as much as we like!
+Even 4 lines if we want, or 5, or 6 or...
+""")
