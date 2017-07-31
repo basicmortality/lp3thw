0e9b4d0c3071ee8737307abdaf9aeff514402af0
basicmortality <info@basicmortality.co.uk>
2017-07-31 20:04:27 +0100

    a conglomeration of stuff from 1 - 10

---
+++ somethingafterex10.py
@@ -0,0 +1,11 @@
+# Things to use:
+# print: print("There are", cars, "cars available.")
+# math: print("Hens", 25 + 30 / 6)
+# variables: with = stings OR = integers
+# print inc' vars with f(ormat): print(f"Let's talk about {my_name}.")
+# print(ing) with dots ("." * 10)
+# or formatted variables: thing = "{} {}", print(thing.format(""))
+# and also with triple quotes!
+# \n new lines, \t indents, and \\ general escaped char$
+#
+#
