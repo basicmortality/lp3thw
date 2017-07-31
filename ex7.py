de79f5728c5403200e70447bf232911171934328
basicmortality <info@basicmortality.co.uk>
2017-07-31 13:10:46 +0100

    ex7.py added, with #statements

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# a standard print statement
+print("Mary had a little lamb.")
+# a print statement with the text in "   " plus {} to place the .format('') text in
+print("Its fleece was white as {}.".format('snow'))
+# another standard print statement
+print("And everywhere that Mary went.")
+# the * 10 bit print 10 (one after the other in a straight line) "......"
+print("." * 10) # what'd that do?
+
+# assigning variables. CheeseBurger! :¬)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print  out the , above, variables without spaces.
+# the empty 'end' just puts a space between 'Cheese' and 'Burger'
+# watch that comma at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end =' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
