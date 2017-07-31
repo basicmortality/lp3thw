e677cfb5af6ad18284a98d6fc80a5f4d4480f88d
basicmortality <info@basicmortality.co.uk>
2017-07-31 18:48:11 +0100

    ex8.py updated with #statements

--- ex8.py
+++ ex8.py
@@ -1,9 +1,16 @@
-formatter = "{} {} {} {}"
+formatter = "{} {} {} {}" # the four {} are spaced containers
+# for the following formated print statements
 
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
+
+# this next, weird one, is telling .format to print "{} {} {} {}" four
+# times (so there'll be twelve of them!).
 print(formatter.format(formatter, formatter, formatter, formatter))
+
+# don't be fooled by the four lines of comma seperated stings
+# they print as spaced strings in the formatter format!
 print(formatter.format(
     "Try your",
     "Own text here",
