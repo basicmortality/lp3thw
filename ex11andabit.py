7b49cbf63687c5a9b9fc53f330d02bdfee698df4
basicmortality <info@basicmortality.co.uk>
2017-08-04 21:51:17 +0100

    added ex11andabit.py

---
+++ ex11andabit.py
@@ -0,0 +1,9 @@
+# reimplimentation of ex11.py, with a different way to write it.
+name = input("What is your name? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("Hello " + name + ", I hope you are well today!")
+print("I see from your info that you are " + age + " years old.")
+print("You weigh " + weight + "kg, and you are " + height + "cm tall.")
