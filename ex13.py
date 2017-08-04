5209a36a5a4d2ae583a1d6cfbffd3c8321d1772d
basicmortality <info@basicmortality.co.uk>
2017-08-04 23:05:32 +0100

    added ex13.py

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+# run this by calling python3.6 and three more args
+# i.s. p3.6 oranges and lemons
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
