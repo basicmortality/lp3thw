af2a7f1c4df1b47e946ed6e9af49d49c43743af4
basicmortality <info@basicmortality.co.uk>
2017-08-04 23:31:48 +0100

    updated ex13andabit.py with comments

--- ex13andabit.py
+++ ex13andabit.py
@@ -1,3 +1,9 @@
+# import 'argv' from module 'sys'
+# run as python3.6 ex13a...py and then something to fill vars one-three
+# as in python3.6 ex13a..py age sex religion
+# i've not print() the first one (script) as that's not needed
+# so it only picks up the last three and outputs v1,v2 and v3
+
 from sys import argv
 
 script, varone, vartwo, varthree = argv
